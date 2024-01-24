4d0e2e5416ab7a73fa6e4b3a166f5f278239123e
kamilatte <118631823+kamilatte@users.noreply.github.com>
2024-01-24 15:26:20 +0200

    Create 404.tsx

---
+++ pages/404.tsx
@@ -0,0 +1,14 @@
+// pages/404.tsx
+
+import React from 'react';
+
+const NotFoundPage: React.FC = () => {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      {/* You can add additional content or styling here */}
+    </div>
+  );
+};
+
+export default NotFoundPage;
