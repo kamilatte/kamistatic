d88317d8ae888081d267a0b7be8e5d734dc37f66
kamilatte <118631823+kamilatte@users.noreply.github.com>
2024-01-24 15:29:20 +0200

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,10 +4,39 @@ import React from 'react';
 
 const NotFoundPage: React.FC = () => {
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-      {/* You can add additional content or styling here */}
-    </div>
+    <>
+      <html lang="en">
+        <head>
+          <meta charSet="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>404: Not Found</title>
+          <style>
+            {`
+              body {
+                font-family: Arial, sans-serif;
+                text-align: center;
+                margin: 20vh auto;
+                color: #333;
+              }
+
+              h1 {
+                font-size: 3em;
+                margin-bottom: 10px;
+              }
+
+              p {
+                font-size: 1.2em;
+                margin: 0;
+              }
+            `}
+          </style>
+        </head>
+        <body>
+          <h1>404: Not Found</h1>
+          <p>The page you are looking for does not exist.</p>
+        </body>
+      </html>
+    </>
   );
 };
 
